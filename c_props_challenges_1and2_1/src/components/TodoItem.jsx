570e4b1b95a2388fc149de2991e4d756ca9341f4
SuperCoder <propscode254@gmail.com>
2023-10-18 13:36:47 +0200

    add destructuring version

--- c_props_challenges_1and2_1/src/components/TodoItem.jsx
+++ c_props_challenges_1and2_1/src/components/TodoItem.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable react/prop-types */
 import "./TodoItem.css";
 
-const TodoItem = (props) => {
-  console.log(props);
+// !Version With PROPS
+
+// const TodoItem = (props) => {
+//   return (
+//     <>
+//       <li className={props.todoObject.doit ? "green" : "red"}>
+//         <h3>{props.todoObject.title}</h3>
+//         <p>{props.todoObject.description}</p>
+//       </li>
+//     </>
+//   );
+// };
+
+//!Version with destructuring
+const TodoItem = ({ done, info, extension }) => {
   return (
     <>
-      <li className={props.todoObject.doit ? "green" : "red"}>
-        <h3>{props.todoObject.title}</h3>
-        <p>{props.todoObject.description}</p>
+      <li className={done ? "green" : "red"}>
+        <h3>{info}</h3>
+        <p>{extension}</p>
       </li>
     </>
   );
--- c_props_challenges_1and2_1/src/components/TodoList.jsx
+++ c_props_challenges_1and2_1/src/components/TodoList.jsx
@@ -2,13 +2,37 @@ import "./TodoList.css";
 import TodoItem from "./TodoItem";
 import { data } from "../db/data";
 
+// !VERSION With PROPS
+
+// const TodoList = () => {
+//     return (
+//       <>
+//         <ul>
+//           <h2>test</h2>
+//           {data.map((todo, id) => (
+//             <TodoItem todoObject={todo} key={id}></TodoItem>
+//           ))}
+//         </ul>
+//       </>
+//     );
+//   };
+
+//   export default TodoList;
+
+//!Version with destructuring
+
 const TodoList = () => {
   return (
     <>
       <ul>
         <h2>test</h2>
-        {data.map((todo, id) => (
-          <TodoItem todoObject={todo} key={id}></TodoItem>
+        {data.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            info={todo.title}
+            extension={todo.description}
+            done={todo.doit}
+          ></TodoItem>
         ))}
       </ul>
     </>
