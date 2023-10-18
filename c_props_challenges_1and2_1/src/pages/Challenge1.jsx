0ea3079f5ec3020ae252eaea943bdde80d7a865d
SuperCoder <propscode254@gmail.com>
2023-10-18 12:01:08 +0200

    add folder

---
+++ c_props_challenges_1and2_1/src/pages/Challenge1.jsx
@@ -0,0 +1,7 @@
+import "./Challenge1.css";
+
+const Challenge1 = () => {
+  return <></>;
+};
+
+export default Challenge1;
---
+++ c_props_challenges_1and2_1/src/pages/Challenge2_1.jsx
@@ -0,0 +1,7 @@
+import "./Challenge2_1.css";
+
+const Challenge2_1 = () => {
+  return <></>;
+};
+
+export default Challenge2_1;
