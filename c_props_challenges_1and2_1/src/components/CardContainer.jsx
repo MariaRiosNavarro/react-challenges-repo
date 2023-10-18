8d6aea6cbf2277df821861b6b7ec05d4b2fd9d5e
SuperCoder <propscode254@gmail.com>
2023-10-18 14:21:29 +0200

    add destructuring version

--- c_props_challenges_1and2_1/src/components/CardContainer.jsx
+++ c_props_challenges_1and2_1/src/components/CardContainer.jsx
@@ -2,12 +2,34 @@ import { productData } from "../db/productData";
 import CardItem from "./CardItem";
 import "./CardContainer.css";
 
+// ! props version
+
+// const CardContainer = () => {
+//   return (
+//     <>
+//       <div className="grid">
+//         {productData.map((product, index) => (
+//           <CardItem productsList={product} key={index} />
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// !destructuring version
+
 const CardContainer = () => {
+  console.log();
   return (
     <>
       <div className="grid">
         {productData.map((product, index) => (
-          <CardItem productsList={product} key={index} />
+          <CardItem
+            key={index}
+            srcPropierty={product.src}
+            namePropierty={product.name}
+            pricePropierty={product.price}
+          />
         ))}
       </div>
     </>
--- c_props_challenges_1and2_1/src/components/CardItem.jsx
+++ c_props_challenges_1and2_1/src/components/CardItem.jsx
@@ -1,13 +1,30 @@
 /* eslint-disable react/prop-types */
 import "./CardItem.css";
 
-const CardItem = (props) => {
+// ! props version
+
+// const CardItem = (props) => {
+//   return (
+//     <>
+//       <div className="card">
+//         <img src={props.productsList.src} alt={props.productsList.name} />
+//         <h3 className="name">{props.productsList.name}</h3>
+//         <h4 className="price">{props.productsList.price}</h4>
+//         <button className="buy-button">BUY NOW</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// !destructuring version
+
+const CardItem = ({ srcPropierty, namePropierty, pricePropierty }) => {
   return (
     <>
       <div className="card">
-        <img src={props.productsList.src} alt={props.productsList.name} />
-        <h3 className="name">{props.productsList.name}</h3>
-        <h4 className="price">{props.productsList.price}</h4>
+        <img src={srcPropierty} alt={namePropierty} />
+        <h3 className="name">{namePropierty}</h3>
+        <h4 className="price">{pricePropierty}</h4>
         <button className="buy-button">BUY NOW</button>
       </div>
     </>
