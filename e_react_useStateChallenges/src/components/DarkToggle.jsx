071a6b642934e9cb5a7fb7d28b7905c5d48dc984
SuperCoder <propscode254@gmail.com>
2023-10-19 14:33:34 +0200

    fix

--- e_react_useStateChallenges/src/components/DarkToggle.jsx
+++ e_react_useStateChallenges/src/components/DarkToggle.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) =>
+    props.theme === "dark" ? "black" : "lightyellow"};
+  color: ${(props) => (props.theme === "dark" ? "lightyellow" : "black")};
+`;
+
+const Button = styled.button`
+  background-color: ${(props) =>
+    props.theme === "dark" ? "black" : "lightyellow"};
+  color: ${(props) => (props.theme === "dark" ? "lightyellow" : "black")};
+  padding: 2rem;
+  border-radius: 5px;
+`;
+
 // !Version mit use State String : useState("light")
-
-// const Wrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   background-color: ${(props) =>
-//     props.theme === "dark" ? "black" : "lightyellow"};
-//   color: ${(props) => (props.theme === "dark" ? "lightyellow" : "black")};
-// `;
-
-// const Button = styled.button`
-//   background-color: ${(props) =>
-//     props.theme === "dark" ? "black" : "lightyellow"};
-//   color: ${(props) => (props.theme === "dark" ? "lightyellow" : "black")};
-//   padding: 2rem;
-//   border-radius: 5px;
-// `;
 
 // const DarkToggle = () => {
 //   const [theme, setTheme] = useState("light");
@@ -39,21 +39,7 @@ import styled from "styled-components";
 //   );
 // };
 
-// Version mit Boolean
-
-const Wrapper = styled.div`
-  background-color: ${(props) =>
-    props.theme === "dark" ? "black" : "lightyellow"};
-  color: ${(props) => (props.theme === "dark" ? "lightyellow" : "black")};
-`;
-
-const Button = styled.button`
-  background-color: ${(props) =>
-    props.theme === "dark" ? "black" : "lightyellow"};
-  color: ${(props) => (props.theme === "dark" ? "lightyellow" : "black")};
-  padding: 2rem;
-  border-radius: 5px;
-`;
+// ! Version mit Boolean useState(false);
 
 const DarkToggle = () => {
   const [dark, setDark] = useState(false);
